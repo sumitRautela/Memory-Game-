01e8504eaec673fd85aa6259323aa6cacedff5d7
Sumit <sumitcool844@gmail.com>
2018-07-17 18:19:44 -0700

    created an array of all the cards

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 /*
  * Create a list that holds all of your cards
  */
+const icons = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", 
+"fa fa-paper-plane-o", "fa fa-anchor", "fa fa-anchor", "fa fa-bolt", "fa fa-bolt",
+ "fa fa-cube", "fa fa-cube", "fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle", "fa fa-bomb", "fa fa-bomb"];
 
 
+const cardsBox = document.querySelector(".collection");
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
