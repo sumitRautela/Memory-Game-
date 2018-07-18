6c5fa858d3cb6786f77a5168249805200dbe3658
Sumit <sumitcool844@gmail.com>
2018-07-18 11:33:59 -0700

    reset functionality added

--- js/app.js
+++ js/app.js
@@ -8,12 +8,8 @@ const icons = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o",
 const cardsBox = document.querySelector(".collection");
 let Cards_opened = [];
 let Cards_matched = [];
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+
+let Reset = document.querySelector(".reset");
 
 
 
@@ -29,7 +25,6 @@ function start() {
         click(card);
     }
 }
-
 
 
 
@@ -107,13 +102,19 @@ function shuffle(array) {
 }
 
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+Reset.addEventListener("click", function() {
+
+    cardsBox.innerHTML = "";
+
+
+    start();
+
+    reset();
+
+});
+
+function reset() {
+
+     Cards_matched = [];
+    
+}
